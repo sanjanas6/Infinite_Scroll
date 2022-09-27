8397b614d6228788e348ffd6a01fd8db762019de
Sanjana Singh <sanjanasingh7774@gmail.com>
2022-09-27 16:40:35 +0530

    Scroll & SearchBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Search from './Search';
 import Pagination from './Pagination';
-import Stories from './Stories';
+import Story from './Stories';
 
 const App = () => {
 
@@ -11,7 +11,7 @@ const App = () => {
     <div>News Website</div>
     {/* <Search />
     <Pagination /> */}
-    <Stories />
+    <Story />
     </>
   )
 }
--- src/Stories.js
+++ src/Stories.js
@@ -1,108 +1,132 @@
-import React, { useState , useEffect } from 'react'
+import React, { useState, useEffect, useRef } from "react";
 // import axios from 'axios'
+// import { Link } from 'react-router-dom';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Stories() {
+  const [list, setList] = useState([]);
+  // const [pageCount , setPageCount] = useState(1)
+  const pageCount = useRef(0);
+  const [searchTitle, setSearchTitle] = useState("");
+  let API = `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=0${pageCount.current}`;
 
-    const [list , setList] = useState([])
-    const [pageCount , setPageCount] = useState(1)
-    const [searchTitle, setSearchTitle] = useState("");
-    
-    let API = `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=0${pageCount}`;  
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch(API);  //retun promise
-            const data = await res.json();
-            
-            setList((prevState)=> {
-                return [...prevState ,...data.hits]
-            })
-            console.log(list);
-        }
-        fetchData();
-        // const interval = setInterval (()=>{
-        //     fetchData() 
-        // }, 2000)
-        // return ()=> clearInterval(interval);
-        
-    },[pageCount]);
+  const fetchData = async () => {
+    console.log(pageCount);
+    const res = await fetch(API);
+    const data = await res.json();
 
-    const loadMore = () => {
-        setPageCount((prevState)=>{
-            return prevState + 1
-        })
-      };
-    window.onscroll = function () {
-        if (
-          window.scrollY + window.innerHeight >= // when it is reaching to end
-          document.documentElement.scrollHeight
-        ) {
-          loadMore(); // i'm calling this function
-        }
-      };
+    setList((prevState) => {
+      return [...prevState, ...data.hits];
+    });
+
+    pageCount.current++;
+    // setPageCount((prevState)=>{
+    //     return prevState + 1
+    // })
+  };
+
+  useEffect(() => {
+    fetchData();
+    const interval = setInterval(fetchData, 10000);
+    return () => clearInterval(interval);
+  }, []);
+
+  // const loadMore = () => {
+  //     setPageCount((prevState)=>{
+  //         return prevState + 1
+  //     })
+  //   };
+  // window.onscroll = function () {
+  //     if (
+  //       window.scrollY + window.innerHeight >= // when it is reaching to end
+  //       document.documentElement.scrollHeight
+  //     ) {
+  //       loadMore(); // i'm calling this function
+  //     }
+  //   };
 
   return (
     <div className='Stories'>
-    <h1 style={{ width: "90%", 
-        height: "25px",
-        // marginRight: "50px",
-        marginLeft: "100px",
-        alignItems: "center"}}>Search Filter</h1>
-    <input
-    style={{
-        width: "90%", 
-        height: "25px",
-        // marginRight: "50px",
-        marginLeft: "100px",
-        alignItems: "center"
-    }}
-    type="text"
-    placeholder='Search...'
-    onChange={(e) => setSearchTitle(e.target.value)}
-    />
-    <h1>{pageCount}</h1>
-    <div>
-        {/* {list && list.length>0 && list.map((story) =>{ */}
-        {list
-         .filter((value) => {
-            if(searchTitle === ""){
-                return value;
-            }else if (
-                value.title.toLowerCase().includes(searchTitle.toLowerCase()))
-            {
-                return value;
-            }else if (
-                value.author.toLowerCase().includes(searchTitle.toLowerCase()))
-            {
-                return value;
-            }
+      <h1
+        style={{
+          width: "90%",
+          height: "25px",
+          // marginRight: "50px",
+          marginLeft: "100px",
+          alignItems: "center",
+        }}
+      >
+        Search Filter
+      </h1>
+      <input
+        style={{
+          width: "90%",
+          height: "25px",
+          // marginRight: "50px",
+          marginLeft: "100px",
+          alignItems: "center",
+        }}
+        type='text'
+        placeholder='Search...'
+        onChange={(e) => setSearchTitle(e.target.value)}
+      />
+      <h1>{pageCount.current}</h1>
+      <InfiniteScroll
+        dataLength={list.length}
+        next={fetchData}
+        hasMore={true}
+        loader={<h2>Loading...</h2>}
+      >
+        {list &&
+          list.length > 0 &&
+          list
+            .filter((story) => {
+              if (searchTitle === "") {
+                return story;
+              } else if (
+                story.title.toLowerCase().includes(searchTitle.toLowerCase())
+              ) {
+                return story;
+              } else if (
+                story.author.toLowerCase().includes(searchTitle.toLowerCase())
+              ) {
+                return story;
+              }
             })
-         .map((story) =>{
-            return (
-        <ul>
-          <div key={story.title}
-          style={{
-            padding: "5px",
-            border: "1px solid green",
-            borderRadius: "10px",
-            backgroundColor: "#ADD8E6",
-            margin: "auto",
-            marginTop: "10px",
-            width: "100%"
-          }}
-          >
-            <h4>Title: {story.title} </h4>
-            <p>Author: <a href="{story.url}">{story.url}</a></p>  
-            <p>Created At: {story.created_at}</p>
-            <p>Tags: {story._tags}</p>
-            <p>Author: {story.author}</p>
-            
-           </div>
-        </ul>
-          );
-         })}
-         </div>
-    </div>
-  )
-}
+            .map((story) => {
+              return (
+                <ul>
+                  <li
+                    style={{
+                      padding: "5px",
+                      border: "1px solid green",
+                      borderRadius: "10px",
+                      backgroundColor: "#ADD8E6",
+                      margin: "auto",
+                      marginTop: "10px",
+                      width: "100%",
+                    }}
+                  >
+                    <h4>Title: {story.title} </h4>
+                    <p>
+                      Author: <a href={story.url}>{story.author}</a>
+                    </p>
+                    <p>Created At: {story.created_at}</p>
+                    <ul>
+                      {story._tags.map((tag) => (
+                        <li key={Math.random() * 10000}>
+                          <p>{tag}</p>
+                        </li>
+                      ))}
+                    </ul>
+                    <p>By: {story.author}</p>
 
+                    {/* <p>Tags: {story._tags}</p> */}
+                  </li>
+                </ul>
+              );
+            })}
+      </InfiniteScroll>
+    </div>
+  );
+}
